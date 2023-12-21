348ed17418ac07d76d7e3d70163ba089bb152a43
mikosis123 <mikosis93@gmail.com>
2023-12-21 15:32:37 -0800

    image addition

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,4 +1,5 @@
 import { TypeAnimation } from "react-type-animation";
+import Image from "next/image";
 import {
   FaInstagram,
   FaTwitter,
@@ -9,7 +10,7 @@ import {
 const Main = () => {
   return (
     <div id="main">
-      <img
+      <Image
         className="w-full h-screen object-cover object-left scale-x-[-1]"
         src="https://images.unsplash.com/photo-1501959181532-7d2a3c064642?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1793&q=80"
         alt=""
