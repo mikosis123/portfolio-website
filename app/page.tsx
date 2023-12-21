69e7286e02cd206024472f447f885437c51dde90
mikosis123 <mikosis93@gmail.com>
2023-12-21 14:21:09 -0800

    projucts and resume

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
+import Work from "./components/Work";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
 
 export default function Home() {
   return (
     <>
       <Sidenav />
       <Main />
+      <Work />
+      <Projects />
+      <Resume />
     </>
   );
 }
