3e99e4d9539582c4da138aba44a108769c3a5805
mikosis123 <mikosis93@gmail.com>
2023-12-21 14:17:17 -0800

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Sidenav from "./components/Sidenav";
+import Main from "./components/Main";
 
 export default function Home() {
   return (
     <>
       <Sidenav />
+      <Main />
     </>
   );
 }
