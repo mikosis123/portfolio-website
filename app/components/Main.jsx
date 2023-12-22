4914660a2c5451cf85cba05e4c264cbe1569abdb
mikosis123 <mikosis93@gmail.com>
2023-12-21 16:56:55 -0800

    animation adjustment

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -8,22 +8,28 @@ import {
 } from "react-icons/fa";
 
 const Main = () => {
+  const texter = {
+    tx1: "I'm Mikiyas Sisay",
+    tx2: "I am ",
+  };
   return (
     <div id="main">
       <Image
         className="w-full h-screen object-cover object-left scale-x-[-1]"
         src="https://images.unsplash.com/photo-1501959181532-7d2a3c064642?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1793&q=80"
         alt=""
+        width={1000}
+        height={1000}
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
         <div className="max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center">
           <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
-            Im Praneet Kashyap
+            {texter.tx1}
           </h1>
           <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
-            Im a
+            {texter.tx2}
             <TypeAnimation
-              sequence={["Student", 1500, "Coder", 1500, "Developer", 1500]}
+              sequence={["Front-End Developer", 1500, "Coder", 1500]}
               wrapper="span"
               speed={50}
               style={{ fontSize: "1em", paddingLeft: "5px" }}
