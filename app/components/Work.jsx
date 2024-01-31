066a5457addd3e428b3791515630061f2d18e893
mikosis123 <mikosis93@gmail.com>
2024-01-31 01:02:19 -0800

    about me

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,46 +1,33 @@
-import React from 'react'
-import WorkItem from './WorkItem'
+import React from "react";
+import WorkItem from "./WorkItem";
 
 const data = [
-    {
-        year: 2010,
-        title: 'JMA',
-        duration: '2 Years',
-        details: 
-        'A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. Though not required by the orthographic conventions of any language with a writing system, paragraphs are a conventional means of organizing extended segments of prose.'
-    },
-    {
-        year: 2012,
-        title: 'RIS',
-        duration: '8 Years',
-        details: 
-        'A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. Though not required by the orthographic conventions of any language with a writing system, paragraphs are a conventional means of organizing extended segments of prose.'
-    },
-    {
-        year: 2020,
-        title: 'BMIS',
-        duration: '2 Years',
-        details: 
-        'A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. Though not required by the orthographic conventions of any language with a writing system, paragraphs are a conventional means of organizing extended segments of prose.'
-    },
-    {
-        year: 2022,
-        title: 'PIET',
-        duration: 'Ongoing...',
-        details: 
-        'A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. Though not required by the orthographic conventions of any language with a writing system, paragraphs are a conventional means of organizing extended segments of prose.'
-    },
-]
+  {
+    year: 2010,
+    title: "Full Stack || Front End Web Developer (MERN Stack)",
+    duration: "2 Years",
+    details:
+      'From the moment I produced "Hello World" in the console of my first application, I knew I was hooked into the world of software development. My specialities include quickly learning new skills and programming languages, problem solving, responsive design principles, website optimization,. So far I have HTML, CSS, Javascript, ReactJS, Typescript,Javascript,TailwindCSS, Bootstrap, Node.JS, Express.JS, Shopify, and Git/GitHub under my belt I also have mechanical engineerig degree. I`m still enthusiastically grabbing onto any other programming languages',
+  },
+];
 
 const Work = () => {
   return (
-    <div id='edu' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-        <h1 className='text-4xl font-bold text-center text-[#001b5e] pb-10'>Education</h1>
-        {data.map((item, idx)=> (
-            <WorkItem key={idx} year={item.year} title={item.title} duration={item.duration} details={item.details}/>
-        ))}
+    <div id="edu" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e] pb-10">
+        ABOUT ME
+      </h1>
+      {data.map((item, idx) => (
+        <WorkItem
+          key={idx}
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
