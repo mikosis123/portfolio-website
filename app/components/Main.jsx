c1366cac1fcde486e8a19a44aef242ca6b06f092
mikosis123 <mikosis93@gmail.com>
2024-02-05 13:47:45 +0300

    svg component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,6 +1,7 @@
 import { TypeAnimation } from "react-type-animation";
+import { Iconmain } from "../assets/Iconmain";
 import Image from "next/image";
-import mikipic from "../assets/mikiyas photo.png";
+import mikipic from "../assets/7922058-removebg-preview.png";
 import {
   FaInstagram,
   FaTwitter,
@@ -17,24 +18,11 @@ const Main = () => {
   };
   return (
     <div id="main">
-      <Image
-        className="w-full h-screen object-cover object-left  scale-x-[-1]"
-        src="https://images.unsplash.com/photo-1501959181532-7d2a3c064642?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1793&q=80"
-        alt=""
-        width={200}
-        height={200}
-      />
-      <div className="w-full h-screen absolute top-0 left-0 bg-white/55 flex flex-col">
-        <Image
-          className="rounded-xl h-[300px] w-[300px] max-w-[700px] m-auto mt-16"
-          src={mikipic}
-          alt=""
-          width={300}
-          height={200}
-        />
+      <div className="w-full h-screen object-cover object-left  "></div>
+      <div className="w-full h-screen absolute top-0 left-0  flex">
         <div className="max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center">
           <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
-            Im Praneet Kashyap
+            {texter.tx1}
           </h1>
           <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
             {texter.tx2}
@@ -69,6 +57,7 @@ const Main = () => {
             </a>
           </div>
         </div>
+        <Image className="h-[500px] my-auto" src={mikipic}></Image>
       </div>
     </div>
   );
