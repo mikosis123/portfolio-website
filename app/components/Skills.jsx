6ce3e70a783583de03254cad9d0eaf97a0d0bc16
mikosis123 <mikosis93@gmail.com>
2024-02-06 03:32:10 -0800

    skill change

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { FaJsSquare, FaReact, FaNodeJs } from "react-icons/fa"; // Importing icons
+
 const skills = [
-  { name: "JavaScript", level: 90 },
-  { name: "React", level: 85 },
-  { name: "Node.js", level: 80 },
+  {
+    name: "JavaScript",
+    level: 90,
+    icon: <FaJsSquare className="text-yellow-500" size={22} />,
+  },
+  {
+    name: "React",
+    level: 85,
+    icon: <FaReact className="text-blue-500" size={22} />,
+  },
+  {
+    name: "Node.js",
+    level: 80,
+    icon: <FaNodeJs className="text-green-500" size={22} />,
+  },
   // Add more skills as needed
 ];
 
@@ -12,17 +26,20 @@ const SkillBar = ({ level }) => (
       className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
       style={{ width: `${level}%` }}
     >
-      {" "}
       {level}%
     </div>
   </div>
 );
+
 const Skills = () => (
   <div className="space-y-4">
     {skills.map((skill, index) => (
-      <div key={index} className="space-y-1">
-        <h4 className="text-lg font-medium">{skill.name}</h4>
-        <SkillBar level={skill.level} />
+      <div key={index} className="flex items-center space-x-2">
+        <div>{skill.icon}</div> {/* Display the icon */}
+        <div className="flex-1 space-y-1">
+          <h4 className="text-lg font-medium">{skill.name}</h4>
+          <SkillBar level={skill.level} />
+        </div>
       </div>
     ))}
   </div>
