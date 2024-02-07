25e567de94a7a6a94e8b530cbf45e1322e21d7cb
mikosis123 <mikosis93@gmail.com>
2024-02-07 16:35:18 +0300

    adjusting work page

--- app/components/WorkItem.jsx
+++ app/components/WorkItem.jsx
@@ -3,7 +3,7 @@ import Skills from "./Skills";
 const WorkItem = ({ year, title, duration, details }) => {
   return (
     <div>
-      <ol className="flex flex-col md:flex-row relative border-l border-blue-200">
+      <ol className="flex flex-col md:flex-row relative border-l border-sky-400">
         <li className="mb-10 ml-4 ">
           <div className="absolute w-3 h-3 bg-blue-300 rounded-full mt-1.5 -left-1.5 border-white" />
           <p className="flex flex-wrap gap-4 flex-row items-center justify-start tex-xs md:text-sm">
