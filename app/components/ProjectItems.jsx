5b9b6eed0949208d8984fa0e3dfa234c7e24fe0a
mikosis123 <mikosis93@gmail.com>
2024-02-08 02:29:14 -0800

    card item change

--- app/components/ProjectItems.jsx
+++ app/components/ProjectItems.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./project.css";
+import Image from "next/image";
+
 
 const ProjectItems = ({ img, title }) => {
-  return <div className="card">ProjectItems</div>;
+  return (
+    <div className="card">
+      <Image src={img}></Image>
+    </div>
+  );
 };
 
 export default ProjectItems;
