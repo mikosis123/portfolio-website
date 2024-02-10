b85b91359e3cf3faa30bdb6f3fe986e9a04f3ce3
mikosis123 <mikosis93@gmail.com>
2024-02-10 22:32:22 +0300

    skills update

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Main />
       <Work />
       <Expriance />
-      {/* <Skills /> */}
+      <Skills />
       <Projects />
       <Resume />
       <Contact />
