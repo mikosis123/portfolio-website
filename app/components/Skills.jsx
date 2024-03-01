0f0ecd3e27ee9f2505324d68126cd09e4389421a
mikosis123 <mikosis93@gmail.com>
2024-03-01 12:03:21 +0300

    adjusting skills for motions

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Redux from "../assets/skillsIcon/Redux";
 import Nexticon from "../assets/skillsIcon/Nexticon";
 import NodeIcon from "../assets/skillsIcon/NodeIcon";
@@ -6,18 +6,103 @@ import Reactlogo from "../assets/skillsIcon/Reactlogo";
 import Tailwind from "../assets/skillsIcon/Tailwind";
 import JavascriptIcon from "../assets/skillsIcon/JavascriptIcon";
 import Typescript from "../assets/skillsIcon/Typescript";
+import FramerChilds from "./FramerChilds";
+import { animate, motion, useMotionValue } from "framer-motion";
+import { useMeasure } from "react-use";
 
 const Skills = () => {
+  const [ref, { width }] = useMeasure(); // Specify type for ref and destructure width
+  const Xtransition = useMotionValue(0);
+
+  useEffect(() => {
+    let control;
+
+    const finalPosition = width / 2 - 8; // Access width from useMeasure
+    control = animate(Xtransition, [0, finalPosition], {
+      ease: "linear",
+      duration: 15,
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+    });
+
+    return () => {
+      control.stop(); // Use function form to stop animation
+    };
+  }, [Xtransition, width]); // Include width in dependencies
+
   return (
-    <div className="flex justify-center item-between">
-      <NodeIcon />
-      <Reactlogo />
-      <Nexticon />
-      <Typescript />
-      <JavascriptIcon />
-      <Redux />
-      <Tailwind />
-    </div>
+    <motion.div
+      className="flex justify-center item-center gap-8 item-between "
+      ref={ref}
+      style={{ x: Xtransition }}
+    >
+      <div className=" flex justify-center item-center gap-10">
+        <FramerChilds>
+          <NodeIcon />
+        </FramerChilds>
+        <FramerChilds>
+          <Reactlogo />
+        </FramerChilds>
+        <FramerChilds>
+          <Nexticon />
+        </FramerChilds>
+        <FramerChilds>
+          <Typescript />
+        </FramerChilds>
+        <FramerChilds>
+          <JavascriptIcon />
+        </FramerChilds>
+        <FramerChilds>
+          <Redux />
+        </FramerChilds>
+        <FramerChilds>
+          <Tailwind />
+        </FramerChilds>
+        <FramerChilds>
+          <NodeIcon />
+        </FramerChilds>
+        <FramerChilds>
+          <Reactlogo />
+        </FramerChilds>
+        <FramerChilds>
+          <Nexticon />
+        </FramerChilds>
+        <FramerChilds>
+          <Typescript />
+        </FramerChilds>
+        <FramerChilds>
+          <JavascriptIcon />
+        </FramerChilds>
+        <FramerChilds>
+          <Redux />
+        </FramerChilds>
+        <FramerChilds>
+          <Tailwind />
+        </FramerChilds>
+        <FramerChilds>
+          <NodeIcon />
+        </FramerChilds>
+        <FramerChilds>
+          <Reactlogo />
+        </FramerChilds>
+        <FramerChilds>
+          <Nexticon />
+        </FramerChilds>
+        <FramerChilds>
+          <Typescript />
+        </FramerChilds>
+        <FramerChilds>
+          <JavascriptIcon />
+        </FramerChilds>
+        <FramerChilds>
+          <Redux />
+        </FramerChilds>
+        <FramerChilds>
+          <Tailwind />
+        </FramerChilds>
+      </div>
+    </motion.div>
   );
 };
 
