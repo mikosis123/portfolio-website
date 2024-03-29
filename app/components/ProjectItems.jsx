e666e4227adcaf5df0644eccae93228ac5fad2ab
mikosis123 <mikosis93@gmail.com>
2024-03-29 11:24:59 +0300

    card allignment fixed

--- app/components/ProjectItems.jsx
+++ app/components/ProjectItems.jsx
@@ -12,16 +12,16 @@ const ProjectItems = ({
 }) => {
   return (
     <div>
-      <div className="card mx-auto z-10 sm:h-[300px] sm:w-[300px] md:h-[350px] md:w-[350px]">
+      <div className="card mx-auto z-10  md:h-[350px] md:w-[350px]">
         <Image
-          className="sm:h-[300px] sm:w-[300px] md:h-[350px] md:w-[350px] object-cover"
-          height={270}
-          width={270}
+          className="h-[300px] w-[300px] md:h-[350px] md:w-[350px] object-cover"
+          height={350}
+          width={350}
           src={img}
         ></Image>
         <div className="card-content">{title}</div>
         <div className="absolute inset-0  flex flex-col items-center justify-center bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100">
-          <p className="text-center text-white text-sm">{description}</p>
+          <p className="text-center text-white p-4 text-sm">{description}</p>
           <div className="flex gap-4 mt-24">
             <Button color="secondary" onClick={() => window.open(liveSiteLink)}>
               Live Site
