3642f6ec40f05f1bcca5072a2e0a9399bd4f76c2
mikosis123 <mikosis93@gmail.com>
2024-04-14 18:53:10 +0300

    eslint removal

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -14,19 +14,19 @@ const Work = () => {
           About me
         </h1>
         <h3>
-          "From the instant I typed 'Hello World' into my console, I was hooked
-          on software development. Originally trained as a mechanical engineer,
-          I pivoted to full stack development, driven by my fascination with
-          building and optimizing digital solutions. I excel at swiftly learning
-          new skills and programming languages, solving problems, and enhancing
-          websites. My toolkit includes HTML, CSS, JavaScript, ReactJS,
-          TypeScript, TailwindCSS, Bootstrap, Node.js, and Express. My fluency
-          extends to Git for version control, RESTful API design, and
-          server-side scripting with Node.js. I wield advanced knowledge in
-          database management systems like MySQL and MongoDB, ensuring robust
-          data handling. With a robust understanding of GitHub workflows, I am
-          primed to deliver excellence in collaborative development
-          environments."
+          &quot;From the instant I typed 'Hello World' into my console, I was
+          hooked on software development. Originally trained as a mechanical
+          engineer, I pivoted to full stack development, driven by my
+          fascination with building and optimizing digital solutions. I excel at
+          swiftly learning new skills and programming languages, solving
+          problems, and enhancing websites. My toolkit includes HTML, CSS,
+          JavaScript, ReactJS, TypeScript, TailwindCSS, Bootstrap, Node.js, and
+          Express. My fluency extends to Git for version control, RESTful API
+          design, and server-side scripting with Node.js. I wield advanced
+          knowledge in database management systems like MySQL and MongoDB,
+          ensuring robust data handling. With a robust understanding of GitHub
+          workflows, I am primed to deliver excellence in collaborative
+          development environments.&quot;
         </h3>
       </div>
 
